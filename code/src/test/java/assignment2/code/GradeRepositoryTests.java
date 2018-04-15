129e773444156e9fb45edf154f0fef84b3a616b7
razPasca <razvan_baws@yahoo.com>
2018-04-15 22:36:12 +0300

    awdsa

---
+++ code/src/test/java/assignment2/code/GradeRepositoryTests.java
@@ -0,0 +1,63 @@
+package assignment2.code;
+
+import assignment2.code.persistance.entity.*;
+import assignment2.code.persistance.repository.EnrolmentRepository;
+import assignment2.code.persistance.repository.GradeRepository;
+import assignment2.code.persistance.repository.StudentRepository;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.Optional;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class GradeRepositoryTests {
+    @Autowired
+    private StudentRepository studentRepository;
+    @Autowired
+    private EnrolmentRepository enrolmentRepository;
+    @Autowired
+    private GradeRepository gradeRepository;
+
+    @Before
+    public void setUp() throws Exception {
+        /*Test saving students*/
+        Student student1 = new Student.Builder().setName("Alice").setEmail("alice@yahoo.com").build();
+        Student student2 = new Student.Builder().setName("Bob").build();
+        Course course1 = new Course("mate1");
+        Course course2 = new Course("mate2");
+
+        student1.addCourse(course1);
+        student1.addCourse(course2);
+
+        student2.addCourse(course1);
+
+        Grade grade = student1.addGrade(5, student1.getEnrolments().get(0));
+
+        //save user, verify has ID value after save
+        assertNull(student1.getId());
+        assertNull(student2.getId());//null before save
+        this.studentRepository.save(student1);
+        this.studentRepository.save(student2);
+        this.gradeRepository.save(grade);
+        assertNotNull(student1.getId());
+        assertNotNull(student2.getId());
+    }
+
+    @Test
+    public void testGradeMethods() {
+        Student student1 = studentRepository.findByEmail("alice@yahoo.com");
+        Enrolment enrolment = student1.getEnrolments().get(0);
+        System.out.println(enrolment);
+        Optional<Grade> grade = gradeRepository.findById(enrolment.getId());
+        assert (grade.get().getGrade() == 5);
+    }
+}
